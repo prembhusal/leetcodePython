344b9286a03305f9f565f6b3087e2ff51aeb581b
prembhusal <remp.bhusal@gmail.com>
2019-05-18 17:35:13 -0400

    Update stringCompress.py

--- stringCompress.py
+++ stringCompress.py
@@ -1,2 +1,23 @@
 #Implement a method to perform basic string compression using the counts of repeated characters. 
 #For example, the string aabcccccaaa would become a2blc5a3.
+
+def stringCompress(s):
+    temp = s[0]
+    count =0
+    out =''
+    index =0
+    for i in s:
+        index +=1
+        if temp ==i:
+            count +=1
+        else:
+            out +=temp+str(count)
+            temp =i
+            count =1
+        
+        if index == len(s):
+            out +=temp+str(count)
+    return out
+    
+
+print stringCompress("aabcccccaaabb")
